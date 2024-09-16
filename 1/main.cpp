1e03a142378ade4ce36c9568cba31c485bcd18b0
martiushev.va <martiushev.va@dozen.mephi.ru>
2024-09-16 10:26:11 +0300

    memory leak detection removed

--- 1/main.cpp
+++ 1/main.cpp
@@ -1,8 +1,3 @@
-#define __CRTDBG_MAP_ALLOC
-#include <crtdbg.h>
-#define DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
-#define new DEBUG_NEW
-//#include <vld.h>
 #include "dial.h"
 using namespace dl;
 
@@ -10,6 +5,5 @@ int main()
 {
     dialog();
     std::cout << "Выход из программы." << std::endl;
-    _CrtDumpMemoryLeaks();
     return 0;
 }
