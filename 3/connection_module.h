27a70176b13d54cd238a372e82299182ea4aec44
martiushev.va <martiushev.va@dozen.mephi.ru>
2024-10-29 01:25:46 +0300

    connection module updated

--- 3/connection_module.h
+++ 3/connection_module.h
@@ -3,11 +3,16 @@
 #include "module.h"
 #include <vector>
 
+struct routeNode {
+    std::shared_ptr<Module> destination;
+    std::shared_ptr<Module> gate;
+};
+
 class ConnectionModule : public Module {
 private:
     int maxSessions_ = 5;
-    std::vector<std::shared_ptr<Module>> activeSessions_ = {};
-    std::vector<std::shared_ptr<Module>> MACs_ = {};
+    std::vector<std::shared_ptr<Module>> sessionList_ = {};
+    std::vector<routeNode> routeList_ = {};
 
 public:
     ConnectionModule() : Module{} {}
@@ -16,15 +21,25 @@ public:
 
     int getMaxSessions() const;
     void setMaxSessions(int maxSessions);
-    std::vector<std::shared_ptr<Module>> getAbleToConnect() const;
-    void setAbleToConnect(std::vector<std::shared_ptr<Module>> ableToConnect);
-    std::vector<std::shared_ptr<Module>> getMACs() const;
-    void setMACs(std::vector<std::shared_ptr<Module>> MACs);
+    std::vector<std::shared_ptr<Module>> getSessionList() const;
+    void setSessionList(std::vector<std::shared_ptr<Module>> sessionList);
+    std::vector<routeNode> getRouteList() const;
+    void setRouteList(std::vector<routeNode> routeList);
 
-    void scanForMACs();
-    void askForMACsOfRelatives(); // expand actual list;
+    void scanForRouteNodes();
+    void askForRouteNodesOfRelatives(); // expand actual list;
     // getter for sharing with other modules;
 
     void establishConnection(Module* module);
     void closeConnection(Module* module);
 };
+
+/*
+sessionList consists of pointers to net modules which can be connected
+to current module directly, at first knee :)
+
+routeList consists of pointers to every possible for connection modules
+even if it cant be connected straightforward and index of certain connection
+from connection table where it need to send package to reach desired destination.
+There is some kind of recursion.
+*/
