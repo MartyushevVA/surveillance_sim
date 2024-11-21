824eae89b48a0984fc30c4b5912c3bf442eebb56
martiushev.va <martiushev.va@dozen.mephi.ru>
2024-11-21 17:44:33 +0300

    input.cpp update to load modules directly to platform

--- 3/implts/input.cpp
+++ 3/implts/input.cpp
@@ -6,84 +6,86 @@
 
 using json = nlohmann::json;
 
-void Game::loadFieldFromFile(const std::string& filename, Environment& environment) {
+std::unique_ptr<Module> loadModule(const json& moduleData) {
+    std::string type = moduleData["type"];
+    int slotsOccupied = moduleData["slotsOccupied"];
+    int energyConsumption = moduleData["energyConsumption"];
+    bool isOn = moduleData["isOn"];
+    int range = moduleData["range"];
+    if (type == "ConnectionModule") {
+        int maxSessions = moduleData["maxSessions"];
+        return std::make_unique<ConnectionModule>(slotsOccupied, energyConsumption, isOn, range, maxSessions);
+    }
+    else if (type == "SensorModule") {
+        std::string sensorType = moduleData["sensorType"];
+        SensorType typeEnum = (sensorType == "XRay") ? SensorType::XRay : SensorType::Optical;
+        return std::make_unique<SensorModule>(slotsOccupied, energyConsumption, isOn, range, typeEnum);
+    }
+    else if (type == "WeaponModule") {
+        std::string chargingDuration = moduleData["chargingDuration"];
+        return std::make_unique<WeaponModule>(slotsOccupied, energyConsumption, isOn, range, chargingDuration);
+    }
+    else;
+}
+
+std::shared_ptr<Platform> loadPlatform(const json& platformData, Environment& environment) {
+    std::string type = platformData["type"];
+    Pair position = {platformData["position"]["x"], platformData["position"]["y"]};
+    std::string description = platformData["description"];
+    int maxEnergyLevel = platformData["maxEnergyLevel"];
+    int slotCount = platformData["slotCount"];
+    int speed = platformData["speed"];
+    if (type == "MobilePlatform") {
+        return std::make_shared<MobilePlatform>(position, &environment, description, maxEnergyLevel, slotCount, speed);
+    }
+    else if (type == "StaticPlatform") {
+        return std::make_shared<StaticPlatform>(position, &environment, description, maxEnergyLevel, slotCount);
+    }
+    else;
+}
+
+void Game::loadFieldFromFile(const std::string& filename) {
     std::ifstream inputFile(filename);
     if (!inputFile.is_open())
         throw std::runtime_error("Could not open file");
     json j;
     inputFile >> j;
     Pair size = {j["field"]["width"], j["field"]["height"]};
-    environment.setSize(size.x, size.y); 
+    environment_.setSize(size.x, size.y); 
 
     for (const auto& obstacleData : j["obstacles"]) {
         Pair position = {obstacleData["position"]["x"], obstacleData["position"]["y"]};
-        auto obstacle = std::make_shared<Obstacle>(position, &environment);
-        environment.addToken(obstacle);
+        auto obstacle = std::make_shared<Obstacle>(position, &environment_);
+        environment_.addToken(obstacle);
     }
 
     for (const auto& intruderData : j["intruders"]) {
         Pair position = {intruderData["position"]["x"], intruderData["position"]["y"]};
-        auto intruder = std::make_shared<Intruder>(position, &environment);
-        environment.addToken(intruder);
+        auto intruder = std::make_shared<Intruder>(position, &environment_);
+        environment_.addToken(intruder);
     }
 
     for (const auto& platformData : j["platforms"]) {
-        std::string type = platformData["type"];
-        Pair position = {platformData["position"]["x"], platformData["position"]["y"]};
-        std::string description = platformData["description"];
-        int maxEnergyLevel = platformData["maxEnergyLevel"];
-        int slotCount = platformData["slotCount"];
-        int speed = platformData["speed"];
-        if (type == "MobilePlatform") {
-            auto platform = std::make_shared<MobilePlatform>(position, &environment, description, maxEnergyLevel, slotCount, speed);
-            environment.addToken(platform);
+        auto platform = loadPlatform(platformData, environment_);
+        environment_.addToken(platform);
+        if (dynamic_cast<StaticPlatform*>(platform.get()))
+        ai_.addConnectedPlatform(platform);
+        if (platformData["modules"]) {
+            for (const auto& moduleData : platformData["modules"]) {
+                auto module = loadModule(moduleData);
+                module->attachTo(platform.get());
+            }
         }
-        else if (type == "StaticPlatform") {
-            auto platform = std::make_shared<StaticPlatform>(position, &environment, description, maxEnergyLevel, slotCount);
-            environment.addToken(platform);
-            ai_.addConnectedPlatform(platform);
-        }
-        else;
     }
 
     for (const auto& moduleData : j["modules"]) {
-        std::string type = moduleData["type"];
-        int slotsOccupied = moduleData["slotsOccupied"];
-        int energyConsumption = moduleData["energyConsumption"];
-        bool isOn = moduleData["isOn"];
-        int range = moduleData["range"];
-        if (type == "ConnectionModule") {
-            int maxSessions = moduleData["maxSessions"];
-            auto module = std::make_unique<ConnectionModule>(slotsOccupied, energyConsumption, isOn, range, maxSessions);
-            if (moduleData["host_position"]) {
-                Pair hostPosition = {moduleData["host_position"]["x"], moduleData["host_position"]["y"]};
-                auto host = dynamic_cast<Platform*>(environment.getToken(hostPosition.x, hostPosition.y).get());
-                if (host)
-                    module->attachTo(host);
-            }
-            else addToStorage(std::move(module));
+        auto module = loadModule(moduleData);
+        if (moduleData["host_position"]) {
+            Pair hostPosition = {moduleData["host_position"]["x"], moduleData["host_position"]["y"]};
+            auto host = dynamic_cast<Platform*>(environment_.getToken(hostPosition.x, hostPosition.y).get());
+            if (host)
+                module->attachTo(host);
         } 
-        else if (type == "SensorModule") {
-            std::string sensorType = moduleData["sensorType"];
-            SensorType typeEnum = (sensorType == "XRay") ? SensorType::XRay : SensorType::Optical;
-            auto module = std::make_unique<SensorModule>(slotsOccupied, energyConsumption, isOn, range, typeEnum);
-            if (moduleData["host_position"]) {
-                Pair hostPosition = {moduleData["host_position"]["x"], moduleData["host_position"]["y"]};
-                auto host = dynamic_cast<Platform*>(environment.getToken(hostPosition.x, hostPosition.y).get());
-                if (host)
-                    module->attachTo(host);
-            }
-            else addToStorage(std::move(module));
-        }
-        else if (type == "WeaponModule") {
-            auto module = std::make_unique<WeaponModule>(slotsOccupied, energyConsumption, isOn, range);
-            if (moduleData["host_position"]) {
-                Pair hostPosition = {moduleData["host_position"]["x"], moduleData["host_position"]["y"]};
-                auto host = dynamic_cast<Platform*>(environment.getToken(hostPosition.x, hostPosition.y).get());
-                if (host)
-                    module->attachTo(host);
-            }
-            else addToStorage(std::move(module));
-        }
+        else addToStorage(std::move(module));
     }
 }
