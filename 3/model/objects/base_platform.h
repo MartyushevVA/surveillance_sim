18661772feb0278ad2f57af5914395babee70567
martiushev.va <martiushev.va@dozen.mephi.ru>
2024-12-05 18:28:15 +0300

    docs + slightly better cmakelists config

--- 3/model/objects/base_platform.h
+++ 3/model/objects/base_platform.h
@@ -4,6 +4,10 @@
 
 class Module;
 
+/**
+ * @class Platform
+ * @brief Class representing a platform that can hold modules and interact with the environment.
+ */
 class Platform : 
     public Placeholder,
     public std::enable_shared_from_this<Platform> {
@@ -13,25 +17,74 @@ protected:
     int maxEnergyLevel_ = 0;
     int slotCount_ = 0;
     std::vector<std::shared_ptr<Module>> modules_ {};
-
+    /**
+     * @brief Constructs a Platform object.
+     * @param position The position of the platform in the environment.
+     * @param environment Pointer to the environment the platform belongs to.
+     * @param description Description of the platform.
+     * @param maxEnergyLevel Maximum energy level of the platform.
+     * @param slotCount Number of slots available for modules.
+     * @param speed Speed of the platform.
+     */
     Platform(Pair position, Environment* environment, std::string description, int maxEnergyLevel, int slotCount, int speed)
         : Placeholder(position, environment, speed), description_(description), maxEnergyLevel_(maxEnergyLevel), slotCount_(slotCount) {}
 
 public:
-    virtual ~Platform() = default;
-    
-    std::string getDescription() const {return description_;}
-    int getEnergyLevel() const {return energyLevel_;}
-    void setEnergyLevel(int energyLevel) {energyLevel_ = energyLevel;}
-    int getMaxEnergyLevel() const {return maxEnergyLevel_;}
-    int getSlotCount() const {return slotCount_;}
-    
-    std::vector<std::shared_ptr<Module>> getModules() const {return modules_;}
+    virtual ~Platform() = default; ///< Destructor for the Platform class.
 
+    /**
+     * @brief Gets the description of the platform.
+     * @return std::string The description of the platform.
+     */
+    std::string getDescription() const { return description_; }
+
+    /**
+     * @brief Gets the current energy level of the platform.
+     * @return int The current energy level.
+     */
+    int getEnergyLevel() const { return energyLevel_; }
+
+    /**
+     * @brief Sets the energy level of the platform.
+     * @param energyLevel The new energy level to set.
+     */
+    void setEnergyLevel(int energyLevel) { energyLevel_ = energyLevel; }
+
+    /**
+     * @brief Gets the maximum energy level of the platform.
+     * @return int The maximum energy level.
+     */
+    int getMaxEnergyLevel() const {return maxEnergyLevel_;}
+
+    /**
+     * @brief Gets the number of slots available for modules.
+     * @return int The number of slots.
+     */
+    int getSlotCount() const { return slotCount_; }
+
+    /**
+     * @brief Gets the list of modules attached to the platform.
+     * @return std::vector<std::shared_ptr<Module>> The list of modules.
+     */
+    std::vector<std::shared_ptr<Module>> getModules() const { return modules_; }
+
+    /**
+     * @brief Installs a module onto the platform.
+     * @param module Pointer to the module to install.
+     * @throws std::runtime_error if the module is not attachable to the platform.
+     */
     void installModule(std::shared_ptr<Module> module);
 
+    /**
+     * @brief Updates the platform and its modules.
+     */
     void update() override;
 
+    /**
+     * @brief Finds a module of a specific type attached to the platform.
+     * @tparam T The type of the module to find.
+     * @return T* Pointer to the found module, or nullptr if not found.
+     */
     template<typename T>
     T* findModuleOfType() const {
         for (const auto& module : getModules())
